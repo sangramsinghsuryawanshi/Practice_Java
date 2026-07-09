ba57116003ee3d22fbad77339e48618a3e0012d0
raj suryawanshi <rajsuryawanshi4100@gmail.com>
2026-07-10 00:40:26 +0530

    hello raj

--- AchieveDimondProblem.java
+++ AchieveDimondProblem.java
@@ -15,7 +15,7 @@ class AchieveDimondProblem implements B
 	}
 	public void hi()
 	{
-		System.out.println("Hi, raj suryawanshi");
+		System.out.println("Hello, raj suryawanshi");
 	}
 	public static void main(String[]ar)
 	{
